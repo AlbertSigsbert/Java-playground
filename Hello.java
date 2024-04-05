d451f6b9ff2415dbaf5ee9b4485c352cfe344fa0
ALBERT <ishengomaalbert@gmail.com>
2024-04-05 11:31:13 +0300

    Variables & literals

--- Hello.java
+++ Hello.java
@@ -1,5 +1,18 @@
 class Hello {
     public static void main (String a[]){
+        int num = 5;
+        byte by =127;
+        short s = 234;
+        long l = 1234452l;
+
+        float f = 5.4f;
+        double d = 5.345;
+
+        char k = 'K';
+
+        boolean b = true;
+
+
         System.out.println("Hello World!");
     }
 }
